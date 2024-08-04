a70348dcb9e06ad4fcfaa058de2a440cec91afaf
Muhammad Saad <saadstudent.cs@gmail.com>
2024-08-04 14:41:15 +0500

    Updated: Jobs Controller

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -1,21 +1,74 @@
+import { StatusCodes } from 'http-status-codes';
+
+import Job from '../models/jobModel.js';
+
+import {
+  BadRequestError,
+  NotFoundError,
+} from '../middlewares/errorMiddlewares.js';
+
 const getAllJobs = async (req, res) => {
-  res.send('Get All Jobs');
+  const jobs = await Job.find({ createdBy: req.user.userId }).sort('createdAt');
+  res.status(StatusCodes.OK).json({ jobs, count: jobs.length });
 };
 
 const getJobById = async (req, res) => {
-  res.send('Get Job By Id');
+  const {
+    user: { userId },
+    params: { id: jobId },
+  } = req;
+
+  const job = await Job.findOne({
+    _id: jobId,
+    createdBy: userId,
+  });
+  if (!job) {
+    throw new NotFoundError(`No job with id ${jobId}`);
+  }
+  res.status(StatusCodes.OK).json({ job });
 };
 
 const createJob = async (req, res) => {
-  res.send('Create Job');
+  req.body.createdBy = req.user.userId;
+  const job = await Job.create(req.body);
+  res.status(StatusCodes.CREATED).json({ job });
 };
 
 const updateJob = async (req, res) => {
-  res.send('Update Job');
+  const {
+    body: { company, position },
+    user: { userId },
+    params: { id: jobId },
+  } = req;
+
+  if (company === '' || position === '') {
+    throw new BadRequestError('Company or Position fields cannot be empty');
+  }
+  const job = await Job.findByIdAndUpdate(
+    { _id: jobId, createdBy: userId },
+    req.body,
+    { new: true, runValidators: true }
+  );
+  if (!job) {
+    throw new NotFoundError(`No job with id ${jobId}`);
+  }
+  res.status(StatusCodes.OK).json({ job });
 };
 
 const deleteJob = async (req, res) => {
-  res.send('Delete Job');
+  const {
+    user: { userId },
+    params: { id: jobId },
+  } = req;
+
+  const job = await Job.findByIdAndRemove({
+    _id: jobId,
+    createdBy: userId,
+  });
+  if (!job) {
+    throw new NotFoundError(`No job with id ${jobId}`);
+  }
+  res.status(StatusCodes.OK).send();
 };
 
-export { getAllJobs, getJobById, createJob, updateJob, deleteJob };
+export { createJob, deleteJob, getAllJobs, getJobById, updateJob };
--- middlewares/errorMiddlewares.js
+++ middlewares/errorMiddlewares.js
@@ -47,5 +47,6 @@ export {
   CustomAPIError,
   errorHandler,
   notFoundHandler,
+  NotFoundError,
   UnauthenticatedError,
 };
